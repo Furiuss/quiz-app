36643a1c0641922889543cb1673aefdec8cf8221
André <andremessias.az@gmail.com>
2022-07-26 11:57:13 -0300

    almost finish all game logic, add finish quizz button and style it

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,26 +1,55 @@
 import { useState, useContext, useEffect } from "react";
 import { Questions } from "../helpers/QuestionBank";
-import {QuizContext} from '../helpers/Contexts'
+import { QuizContext } from "../helpers/Contexts";
 
 export const Quiz = () => {
-  const {score, setScore} = useContext(QuizContext)
+  const { score, setScore, setGameState } = useContext(QuizContext);
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
-  
-  
+
+  const nextQuestion = () => {
+    if (Questions[currentQuestion].answer == optionChosen) {
+      setScore(score + 1);
+    }
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const finishQuiz = () => {
+    if (Questions[currentQuestion].answer == optionChosen) {
+      setScore(score + 1);
+    }
+
+    setGameState("endScreen");
+  };
 
   return (
     <div className="Quiz">
       <h1>{Questions[currentQuestion].prompt}</h1>
       <div className="options">
-        <button onClick={() => setOptionChosen("A")}>{Questions[currentQuestion].optionA}</button>
-        <button onClick={() => setOptionChosen("B")}>{Questions[currentQuestion].optionB}</button>
-        <button onClick={() => setOptionChosen("C")}>{Questions[currentQuestion].optionC}</button>
-        <button onClick={() => setOptionChosen("D")}>{Questions[currentQuestion].optionD}</button>
+        <button onClick={() => setOptionChosen("A")}>
+          {Questions[currentQuestion].optionA}
+        </button>
+        <button onClick={() => setOptionChosen("B")}>
+          {Questions[currentQuestion].optionB}
+        </button>
+        <button onClick={() => setOptionChosen("C")}>
+          {Questions[currentQuestion].optionC}
+        </button>
+        <button onClick={() => setOptionChosen("D")}>
+          {Questions[currentQuestion].optionD}
+        </button>
       </div>
 
-      <button className="nextQuestion-btn">Next Question</button>
+      {currentQuestion == Questions.length - 1 ? (
+        <button onClick={finishQuiz} className="finish-btn">
+          Finish Quiz
+        </button>
+      ) : (
+        <button onClick={nextQuestion} className="nextQuestion-btn">
+          Next Question
+        </button>
+      )}
     </div>
   );
 };
