16840b609755c9eeeb179645ac122008a1bb7867
André <andremessias.az@gmail.com>
2022-07-26 00:16:44 -0300

    add question data

---
+++ src/helpers/QuestionBank.jsx
@@ -0,0 +1,26 @@
+export const Question = [
+  {
+    prompt: "What is my name?",
+    optionA: "Erick",
+    optionB: "André",
+    optionC: "Matheus",
+    optionD: "Filipe",
+    answer: "optionC"
+  },
+  {
+    prompt: "How many mundial titles does Santos have?",
+    optionA: "2",
+    optionB: "3",
+    optionC: "1",
+    optionD: "0",
+    answer: "optionA"
+  },
+  {
+    prompt: "What is my favorite anime?",
+    optionA: "FullMetal",
+    optionB: "Gurren Lagan",
+    optionC: "Mob Psycho",
+    optionD: "One Piece",
+    answer: "optionD"
+  },
+]
