53cd89507a06db2680cd8889f3350a923cc85d8e
André <andremessias.az@gmail.com>
2022-07-26 16:02:39 -0300

    finish all logic

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {useContext} from 'react'
+import {QuizContext} from '../helpers/Contexts'
 
 export const EndScreen = () => {
+  const {score, setScore, setGameState} = useContext(QuizContext)
+
+  const resetGame = () => {
+    setScore(0)
+    setGameState("menu")
+  }
+
   return (
-    <div>EndScreen</div>
+    <div className="End">
+      <h1>Quiz finished</h1>
+      <h1>{score}/3</h1>
+      <button onClick={resetGame}>Restart Quiz</button>
+    </div>
   )
 }
--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,12 +1,16 @@
-import {useContext} from 'react'
-import {QuizContext} from '../helpers/Contexts'
+import { useContext } from "react";
+import { QuizContext } from "../helpers/Contexts";
 
 export const MainMenu = () => {
-  const {gameState, setGameState} = useContext(QuizContext)
+  const { gameState, setGameState } = useContext(QuizContext);
 
   return (
     <div className="Menu">
-      <button onClick={() => setGameState("quiz")}>Start Quiz</button>
+      <form  onSubmit={() => setGameState("quiz")}>
+        <label>Enter your name</label>
+        <input type="text" placeholder="Ex. John Wick"/>
+        <button type="submit">Start Quiz</button>
+      </form>
     </div>
-  )
-}
+  );
+};
